0245a50e72a758a82a88edb79eed093cbfbd7b18
rafmontenegro <rafaelcouto111@gmail.com>
2024-04-02 12:47:58 -0300

    feat: delete node at n position

--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -28,10 +28,10 @@ void Insert(int data, int n) {
 
     temp1 -> next = temp2 -> next;
     temp2 -> next = temp1;
-
 };
 
 void Print() {
+
     struct Node* temp = head;
     while(temp != NULL){
         printf("%d", temp -> data);
@@ -39,17 +39,41 @@ void Print() {
     };
 
     printf("\n");
+};
 
+void Delete (int n){
+
+    struct Node* tempDelete = head;
+
+    if(n == 1) {
+        head = tempDelete -> next;
+        free(tempDelete);
+        return;
+    }
+
+    for(int i = 0; i < n-2; i++){
+        tempDelete = tempDelete -> next;
+    }
+
+    struct Node* tempDelete2 = tempDelete -> next;
+    tempDelete -> next = tempDelete2 -> next;
+    free(tempDelete2);
 };
 
 int main() {
 
     head = NULL;
 
-    Insert(2, 1);
-    Insert(3, 2);
     Insert(6, 1);
+    Insert(6, 2);
+    Insert(3, 1);
+    Insert(9, 3);
     Insert(9, 2);
     Print();
 
+    int n;
+    printf("Enter a position\n");
+    scanf("%d", &n);
+    Delete(n);
+    Print();
 }
